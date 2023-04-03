d6905fa9fe43b420158a0f09e0c48665c9ef5122
Kalinina Oksana <oksana108@gmail.com>
2023-04-03 21:45:35 +0400

    add game prime

---
+++ bin/brain-prime.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import playGame from '../src/games/primeGame.js';
+
+playGame();
---
+++ src/games/primeGame.js
@@ -0,0 +1,28 @@
+import {
+  MIN_RANGE, MAX_RANGE, TRIES, getRandomInt, playGame,
+} from '../index.js';
+
+const isPrime = (number) => {
+  for (let i = 2; i < number; i += 1) {
+    if (number % i === 0) {
+      return 'no';
+    }
+  }
+  return 'yes';
+};
+
+export default () => {
+  const questionText = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+  const gameLevels = [];
+
+  for (let i = 0; i < TRIES; i += 1) {
+    const number = getRandomInt(MAX_RANGE, MIN_RANGE);
+
+    const task = (`${number}`);
+
+    const correctAnswer = isPrime(number);
+
+    gameLevels.push([task, correctAnswer]);
+  }
+  playGame(questionText, gameLevels);
+};
